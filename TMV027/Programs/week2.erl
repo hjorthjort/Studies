2e91e5a029f7354b5c686ced30449ab29e35934b
hjorthjort <rikard.hjort@gmail.com>
2016-04-07 22:39:45 +0200

    Validate task 3

--- TMV027/Programs/week2.erl
+++ TMV027/Programs/week2.erl
@@ -19,6 +19,11 @@ has_011(Strings) ->
 end_in_0100(Strings) ->
     do(Strings, fun delta0100/2, [q4]).
 
+has_bba(Strings) ->
+    do(Strings, fun delta_bba/2, [q3]).
+
+has_no_bba(Strings) ->
+    do(Strings, fun delta_bba/2, [q0,q1,q2]).
 
 delta00(q0, "1") ->
     q0;
@@ -79,4 +84,42 @@ delta011(State, "1") ->
             q3;
         q3 ->
             q3
+    end.
+
+%--------------------
+delta0100(State, "0") ->
+    case State of
+        q0 -> q1;
+        q1 -> q1;
+        q2 -> q3;
+        q3 -> q4;
+        q4 -> q1
+    end;
+
+delta0100(State, "1") ->
+    case State of
+        q0 -> q0;
+        q1 -> q2;
+        q2 -> q0;
+        q3 -> q2;
+        q4 -> q2
+    end.
+
+%--------------------
+
+delta_bba(q3, _) ->
+    q3;
+
+delta_bba(State, "a") ->
+    case State of
+        q0 -> q0;
+        q1 -> q0;
+        q2 -> q3
+    end;
+
+delta_bba(State, "b") ->
+    case State of
+        q0 -> q1;
+        q1 -> q2;
+        q2 -> q2
     end.
