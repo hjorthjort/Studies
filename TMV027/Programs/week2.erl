7dc842ebb67c94d58b5ecae14f8cd4ec70b7918f
hjorthjort <rikard.hjort@gmail.com>
2016-04-07 21:24:40 +0200

    Book exercise 2.2.4 in erlang

--- TMV027/Programs/week2.erl
+++ TMV027/Programs/week2.erl
@@ -6,6 +6,11 @@ end_in_00(Strings) ->
     Automata = automata:new_automata(fun(State, Char) -> delta00(State,Char) end, q0, [q2]),
     automata:offer(Strings, Automata).
 
+has_000(Strings) ->
+    Automata = automata:new_automata(fun delta000/2, q0, [q3]),
+    automata:offer(Strings, Automata).
+
+
 delta00(q0, "1") ->
     q0;
 
@@ -24,3 +29,21 @@ delta00(q2, "1") ->
 delta00(q2, "0") ->
     q2.
 
+delta000(State, "0") ->
+    case State of 
+        q0 ->
+            q1;
+        q1 ->
+            q2;
+        q2 ->
+            q3;
+        q3 ->
+            q3
+    end;
+
+delta000(q3, "1") ->
+    q3;
+
+delta000(_, "1") ->
+    q0.
+    
