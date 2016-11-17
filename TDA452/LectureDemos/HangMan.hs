d8a7b562975aa16a9ce4bd0cca3a2a69b102235b
hjorthjort <rikard.hjort@gmail.com>
2016-11-17 16:43:19 +0100

    Finsih up hangman

--- TDA452/LectureDemos/HangMan.hs
+++ TDA452/LectureDemos/HangMan.hs
@@ -2,17 +2,33 @@ import Data.List
 import System.Random
 
 wordFile = "/usr/share/dict/words"
-guessLimit = 12
+guessLimit word = 2 * length word
 
 main :: IO ()
 main = do
     word <- randomWord
-    -- For debgging, print the word.
+    -- For debugging, print the word.
     putStrLn word
     gameLoop word []
 
 gameLoop :: String -> [Char] -> IO ()
-gameLoop _ _ = putStrLn "Out of guesses"
+gameLoop word guesses | guessesLeft word guesses <= 0 = putStrLn "Out of guesses"
+gameLoop word guesses 
+  | correct word guesses = putStrLn "Correct!"
+  | otherwise = do
+      putStrLn "Guess again"
+      putStrLn $ ("Guesses left: " ++) $ show $ guessesLeft word guesses
+      putStrLn $ status word guesses
+      putStrLn $ "Failed: " ++ (guesses \\ word)
+      read <- getLine
+      let readFiltered = read \\ guesses
+      gameLoop word $ guesses ++ readFiltered
+
+correct :: String -> [Char] -> Bool
+correct word chars = word \\ chars == ""
+
+status :: String -> [Char] -> String
+status word guesses = map (\c -> if c `elem` guesses then c else '_') word
 
 randomWord :: IO String
 randomWord = do
@@ -27,3 +43,5 @@ pickRandom xs = do
     let (randInt, _) = randomR (0, length xs - 1) rand
     return $ xs !! randInt
 
+guessesLeft :: String -> [Char] -> Int
+guessesLeft word guesses = 12 - length (guesses \\ word)
