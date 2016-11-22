c4e963b4f91b09f4321a33248bdc57d76122c0a4
hjorthjort <rikard.hjort@gmail.com>
2016-11-22 14:27:54 +0100

    Working parser, but without higher order functions

--- TDA452/LectureDemos/Parser.hs
+++ TDA452/LectureDemos/Parser.hs
@@ -1,11 +1,48 @@
+module Parser
+    where
+
+import ArithmeticQuiz
+import Data.Char
+
 u = undefined
+
+expr :: Parser Expr
+parse = expr . filter (not . isSpace)
 
 -- BNF-style grammar
 -- <expression> ::= <term> | <term> "+" <expression>
-expr s = u
+expr :: Parser Expr
+expr s = case term s of
+           Just (t, '+':s') -> case expr s' of
+                                 Just (e, s'')  -> Just (t `Add` e, s'')
+                                 Nothing        -> Just (t, '+':s')
+           r                -> r
 
 -- <term>       ::= <factor> | <factor> "*" <term>
-term s = u
+term :: Parser Expr
+term s = case factor s of
+           Just (n, '*':s') -> case term s' of
+                                 Just (n', s'') -> Just (n `Mul` n', s'')
+                                 Nothing        -> Just (n, '*':s')
+           r                -> r
 
--- <factor>     ::= "(" <expression ")" | <number>
-factor = u
+-- <factor>     ::= "(" <expression> ")" | <number>
+factor :: Parser Expr
+factor ('(':s) = case expr s of
+                   Just (e, ')':s') -> Just (e, s')
+                   _                -> Nothing
+factor s = num s
+
+-- warm up: parse n1+n2+...+nk for k>=1
+expr1' s = case num s of
+           Just (n, '+':s')    -> case expr1' s' of
+                                     Just (n', s'') -> Just (n `Add` n', s'')
+                                     Nothing        -> Just (n, '+':s')
+           r                   -> r
+
+type Parser a = String -> Maybe (a, String)
+
+num :: Parser Expr
+num s = case reads s of
+          (i,s'):_  -> Just (Num i, s')
+          _         -> Nothing
