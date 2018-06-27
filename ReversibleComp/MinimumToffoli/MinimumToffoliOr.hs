572212321964c2cf506f4afd76264a164efaeb25
hjorthjort <rikard.hjort@gmail.com>
2018-06-27 21:43:45 +0900

    Save some changes from reversible cmputation course

--- ReversibleComp/MinimumToffoli/MinimumToffoliOr.hs
+++ ReversibleComp/MinimumToffoli/MinimumToffoliOr.hs
@@ -1,8 +1,11 @@
 main = do
      putStrLn $ show $ implementsOr mySolution
 
+type Input = (Integer, Integer, Integer)
+type Output = (Integer, Integer, Integer)
+type Gate = Input -> Output
 -- A Toffoli gate.
-toffoli :: (Integer, Integer, Integer) -> (Integer, Integer, Integer)
+toffoli :: Gate
 toffoli (a, b, c) = if a == 1 && b == 1 then (1, 1, 1 - c) else (a, b, c)
 
 setA a = \b c -> toffoli (a, b, c)
@@ -12,6 +15,12 @@ setC c = \a b -> toffoli (a, b, c)
 andGate = setC 0
 xorGate = setA 1
 
+permute3tuple (a, b, c) = [(a, b, c), (a, c, b), (b, a, c), (b, c, a), (c, a, b), (c, b, a)]
+
+permute :: Input -> [Output]
+permute = \input -> [toffoli perm | perm <- permute3tuple input]
+
+-- Checking
 -- Truth table.
 -- Check that a function implements OR.
 implements :: [[Integer]] -> (Integer -> Integer -> Integer) -> Bool
@@ -20,7 +29,6 @@ implements table f = and $ map (\row -> f (row !! 0) (row !! 1) == (row !! 2)) t
 orTable = [[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]]
 
 implementsOr = implements orTable
-
 
 -- Known working implementation
 mySolution a b =
